991a0547a22cca1d7dca81403029032eb2a253f4
ST <email@sten-schmidt.net>
2019-03-14 20:14:27 +0100

    Small JS improvements

--- BuildExamples.ps1
+++ BuildExamples.ps1
@@ -1,2 +1,3 @@
 Remove-Item .\Examples\*.pdf
 node html2pdf.js --html=Examples/Example1/index.html --css=Examples/Example1/more.css --pdf=Examples/Example1.pdf
+node html2pdf.js --html=Examples/Example1/index.html --pdf=Examples/Example_NoCss.pdf
