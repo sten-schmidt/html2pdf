36ae512ab0f34d3a64ac41db7f21d0429227ed9f
ST <email@sten-schmidt.net>
2019-05-12 08:36:42 +0200

    tests added

--- Tests/CheckSum/CheckSumTests.js
+++ Tests/CheckSum/CheckSumTests.js
@@ -2,9 +2,17 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const CheckSum_1 = require("../../Tools/CheckSum");
 describe('CheckSumTests', () => {
-    it('Test Simple Strings', () => {
+    it('Calc from EmptyString', () => {
         let value = new CheckSum_1.CheckSum().getCheckSum('', 'sha1', 'hex');
         expect(value).toBe('da39a3ee5e6b4b0d3255bfef95601890afd80709');
+    });
+    it('Calc from Letter A', () => {
+        let value = new CheckSum_1.CheckSum().getCheckSum('A', 'sha1', 'hex');
+        expect(value).toBe('6dcd4ce23d88e2ee9568ba546c007c63d9131c1b');
+    });
+    it('Calc from Letter B', () => {
+        let value = new CheckSum_1.CheckSum().getCheckSum('a', 'sha1', 'hex');
+        expect(value).toBe('86f7e437faa5a7fce15d1ddcb9eaeaea377667b8');
     });
 });
 //# sourceMappingURL=CheckSumTests.js.map
--- Tests/CheckSum/CheckSumTests.ts
+++ Tests/CheckSum/CheckSumTests.ts
@@ -1,10 +1,23 @@
 ﻿import { CheckSum } from '../../Tools/CheckSum';
 
 describe('CheckSumTests', () => {
-    it('Test Simple Strings', () => {
+
+    it('Calc from EmptyString', () => {
 
         let value = new CheckSum().getCheckSum('', 'sha1', 'hex');
         expect(value).toBe('da39a3ee5e6b4b0d3255bfef95601890afd80709');
+    });
+
+    it('Calc from Letter A', () => {
+
+        let value = new CheckSum().getCheckSum('A', 'sha1', 'hex');
+        expect(value).toBe('6dcd4ce23d88e2ee9568ba546c007c63d9131c1b');
+    });
+
+    it('Calc from Letter B', () => {
+
+        let value = new CheckSum().getCheckSum('a', 'sha1', 'hex');
+        expect(value).toBe('86f7e437faa5a7fce15d1ddcb9eaeaea377667b8');
     });
 
 });
